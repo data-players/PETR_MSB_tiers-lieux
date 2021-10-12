e3b2570878dba77e5d0229cf78e3149675d2e88c
simon <simon.louvet.zen@gmail.com>
2021-10-12 10:41:39 +0200

    factor inputs equipments

--- frontend/src/resources/Equipment/EquipmentEdit.js
+++ frontend/src/resources/Equipment/EquipmentEdit.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import { Edit } from "@semapps/archipelago-layout";
 import EquipmentTitle from './EquipmentTitle';
+import EquipmentInputs from './EquipmentInputs';
 import {
-  TextInput,
-  ArrayInput,
-  BooleanInput,
-  FormTab,
-  ImageInput,
-  NumberInput,
-  SelectInput,
-  SimpleFormIterator,
-  TabbedForm,
-  ListContextProvider,
-  Datagrid,
-  TextField,
-  EditButton,
-  Button,
   SimpleForm,
-  SaveButton,
-  SaveContextProvider,
-  CreateContextProvider,
-  EditContextProvider,
-  FormWithRedirect,
-  ReferenceInput
 } from 'react-admin';
 import { MarkdownInput } from '@semapps/markdown-components';
 import { useLocation } from 'react-router';
@@ -35,39 +16,7 @@ export const EquipmentEdit = props =>{
   return (
     <Edit title={<EquipmentTitle />} {...props} >
       <SimpleForm redirect={query.redirectUri}>
-        <ReferenceInput
-          source="petr:equipmentOfferedBy"
-          reference="Organization"
-          disabled
-          fullWidth
-        >
-          <SelectInput optionText="pair:label" />
-        </ReferenceInput>
-        <ReferenceInput
-          source="petr:hasEquipmentType"
-          reference="EquipmentType"
-          fullWidth
-        >
-          <SelectInput optionText="pair:label" />
-        </ReferenceInput>
-        <MarkdownInput source="pair:description" multiline fullWidth />
-        <TextInput source="petr:model" fullWidth />
-        <NumberInput source="petr:amount" defaultValue={1} fullWidth />
-        <ReferenceInput
-          source="petr:hasRate"
-          reference="Rate"
-          fullWidth
-        >
-          <SelectInput optionText="pair:label" />
-        </ReferenceInput>
-        <TextInput source="petr:availablity" fullWidth />
-        <ReferenceInput
-          source="petr:hasAccessModality"
-          reference="AccessModality"
-          fullWidth
-        >
-          <SelectInput optionText="pair:label" />
-        </ReferenceInput>
+        <EquipmentInputs/>
       </SimpleForm>
     </Edit>
   )
