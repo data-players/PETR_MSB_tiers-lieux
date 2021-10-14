e26436f773f4c8fd72da356839b69d067eaa7fee
simon <simon.louvet.zen@gmail.com>
2021-10-14 02:57:58 +0200

    super admin simon

--- middleware/services/webacl.service.js
+++ middleware/services/webacl.service.js
@@ -4,7 +4,7 @@ module.exports = {
   mixins: [WebAclService],
   settings: {
     baseUrl: process.env.SEMAPPS_HOME_URL,
-    superAdmins: ['http://localhost:3000/persons/v.farcy72','http://localhost:3000/persons/simon.louvet.zen','https://data.petr-msb.data-players.com/persons/v.farcy72','https://data.petr-msb.data-players.com/simon.louvet.zen']
+    superAdmins: ['http://localhost:3000/persons/v.farcy72','http://localhost:3000/persons/simon.louvet.zen','https://data.petr-msb.data-players.com/persons/v.farcy72','https://data.petr-msb.data-players.com/persons/simon.louvet.zen']
   },
   actions: {
     async addRightsToAll(ctx) {
