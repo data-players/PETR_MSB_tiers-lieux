1ca8f2ce8e442f946b8efce2e663aa9f543756de
VincentFarcy <v.farcy72@gmail.com>
2021-10-19 11:41:45 +0200

    disable delete button

--- frontend/src/resources/Person/PersonEdit.js
+++ frontend/src/resources/Person/PersonEdit.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { SaveButton, SimpleForm, TextInput } from "ra-ui-materialui";
+import { Toolbar } from 'react-admin';
 import { Edit } from "@semapps/archipelago-layout";
-import { SimpleForm, TextInput } from "ra-ui-materialui";
 import PersonTitle from './PersonTitle';
 import MarkdownInput from '../../markdown/MarkdownInput'
 
+const NoDeleteToolBar = props => (
+  <Toolbar {...props} >
+    <SaveButton redirect="list" />
+  </Toolbar>
+)
+
 export const PersonEdit = props => (
-    <Edit title={<PersonTitle />} {...props} >
-        <SimpleForm redirect="list" >
-            <TextInput source="pair:label" fullWidth />
-            <MarkdownInput multiline source="pair:description" fullWidth />
-        </SimpleForm>
-    </Edit>
+  <Edit title={<PersonTitle />} {...props} >
+    <SimpleForm toolbar={<NoDeleteToolBar />}  redirect="list" >
+      <TextInput source="pair:label" fullWidth />
+      <MarkdownInput multiline source="pair:description" fullWidth />
+    </SimpleForm>
+  </Edit>
 )
 
 export default PersonEdit;
