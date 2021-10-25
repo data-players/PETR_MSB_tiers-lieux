60efc58d0f7370c82d25298b9fc8bf79d96179b3
VincentFarcy <v.farcy72@gmail.com>
2021-10-25 12:25:12 +0200

    display none

--- frontend/src/resources/Organization/OrganizationShow/index.js
+++ frontend/src/resources/Organization/OrganizationShow/index.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
 
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import { useShowController } from 'react-admin';
 import { BreadcrumbsItem } from 'react-breadcrumbs-dynamic';
 
 import { Show } from 'react-admin';
 import { ShowActions } from "@semapps/archipelago-layout";
 
-
 import OrganizationShowContactLayout from './OrganizationShowContactLayout';
 import OrganizationShowMainLayout from './OrganizationShowMainLayout';
 import OrganizationShowNavLayout from './OrganizationShowNavLayout';
 
+const useStyles = makeStyles((theme) => ({
+  showContainer: {
+    "& #react-admin-title": {
+      display: 'none',
+    }
+  }
+}));
+
 const OrganizationShow = ({...props}) => {
+  const classes = useStyles();
   const { record } = useShowController(props);
   const state = useSelector(state => state);
   const isAdminContext = state.customState.isAdminContext;
   const currentUri = useLocation().pathname;
-  
   return (
     <>
       { ! isAdminContext &&
@@ -27,7 +34,7 @@ const OrganizationShow = ({...props}) => {
           <BreadcrumbsItem to='/Map'>Cartographie</BreadcrumbsItem>
         </>
       }
-      <Show actions={<ShowActions hasList={false} />} {...props} >
+      <Show className={classes.showContainer} actions={<ShowActions hasList={false} />} {...props}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={3}>
             <OrganizationShowNavLayout {...props} />
