17674baf1f781f9f3bad7fc90d7b1738da20710f
bastien.siguier <bastien.siguier@gmail.com>
2023-03-22 23:09:55 +0100

    fix some label

--- frontend/src/resources/Person/PersonEdit.js
+++ frontend/src/resources/Person/PersonEdit.js
@@ -15,7 +15,7 @@ export const PersonEdit = props => (
   <Edit title={<PersonTitle />} {...props} >
     <SimpleForm toolbar={<NoDeleteToolBar />}  redirect="list" >
       <TextInput source="pair:label" fullWidth />
-      <MarkdownInput multiline source="pair:description" fullWidth />
+      <MarkdownInput label="Description" multiline source="pair:description" fullWidth />
     </SimpleForm>
   </Edit>
 )
